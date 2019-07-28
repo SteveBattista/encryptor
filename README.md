f039fe613bd1a647a8266f0457243ced82cd29ee
Stephen Battista <stephen.battista@gmail.com>
2019-07-28 09:31:05 -0400

    Upading for lastest ring

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-[![Safety Dance](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/) 
+[![Safety Dance](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
 
 Does not test nonce advance as we want to try an unlimited number of attempts. Uses less_safe_key in aead.
+
+Built with untrusted = "0.6.2"  Using "0.7.0" causes errors
+Built with ring = "0.16.5"
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@
 use ring::aead::*;
 use rand::*;
 use ring::digest::{Context, SHA256, SHA384, SHA512, SHA512_256};
-use ring::agreement::{agree_ephemeral, EphemeralPrivateKey, UnparsedPublicKey};
 use ring::agreement::{ECDH_P256,ECDH_P384,X25519};
 use ring::rand as ringrand;
 use data_encoding::BASE64;
 
 
 
-fn test_aead(key : &[u8], data :&[u8], algo: &'static ring::aead::Algorithm ) {
+fn test_aead(key : &[u8], data :&[u8], datalength : usize, algo: &'static ring::aead::Algorithm ) {
 
     // Ring uses the same input variable as output
     let mut in_out = data.to_vec();
@@ -23,11 +22,12 @@ fn test_aead(key : &[u8], data :&[u8], algo: &'static ring::aead::Algorithm ) {
     }
 
     // Opening key used to decrypt data
-
-    let opening_key = OpeningKey::new(&algo, key).unwrap();
+    let unboud_key = ring::aead::UnboundKey::new(algo,key).unwrap();
+    let less_safe_key = ring::aead::LessSafeKey::new(unboud_key);
+    //Nonce::assume_unique_for_key(*nonce_byte),
 
     // Sealing key used to encrypt data
-    let sealing_key = SealingKey::new(&algo, key).unwrap();
+    //let sealing_key = SealingKey::new(&algo, key).unwrap();
 
     // Random nonce is first 12 bytes of a hash of the key
     let nonce_byte = &mut [0; 12];
@@ -36,57 +36,49 @@ fn test_aead(key : &[u8], data :&[u8], algo: &'static ring::aead::Algorithm ) {
     nonce_byte.copy_from_slice(&context.finish().as_ref()[0..12]);
 
     // Encrypt data into in_out variable
-    seal_in_place(
-        &sealing_key,
-        Nonce::assume_unique_for_key(*nonce_byte),
+    ring::aead::LessSafeKey::seal_in_place_append_tag(
+        &less_safe_key ,
+        ring::aead::Nonce::assume_unique_for_key(*nonce_byte),
         Aad::empty(),
-        &mut in_out,
-        algo.tag_len(),
+        &mut in_out
     )
     .unwrap();
 
     // println!("Encrypted data's size {}", output_size);
 
-    let decrypted_data = open_in_place(
-        &opening_key,
-        Nonce::assume_unique_for_key(*nonce_byte),
+    let decrypted_data = ring::aead::LessSafeKey::open_in_place(
+        &less_safe_key,
+        ring::aead::Nonce::assume_unique_for_key(*nonce_byte),
         Aad::empty(),
-        0,
         &mut in_out,
     )
     .unwrap();
 
-    //println!("{}", BASE64.encode(data));
-    //println!("{}", BASE64.encode(decrypted_data));
-    assert_eq!(data, decrypted_data);
+    println!("{}", BASE64.encode(&data[..]));
+    println!("{}", BASE64.encode(&decrypted_data[..datalength]));
+    assert_eq!(data[..], decrypted_data[..datalength]);
 
 }
 fn test_agreement(data :&[u8], algo: &'static ring::agreement::Algorithm ) {
     let rng = ringrand::SystemRandom::new();
 
-let my_private_key = EphemeralPrivateKey::generate(&algo, &rng).unwrap();
+let my_private_key = ring::agreement::EphemeralPrivateKey::generate(&algo, &rng).unwrap();
 
 // Make `my_public_key` a byte slice containing my public key. In a real
 // application, this would be sent to the peer in an encoded protocol
 // message.
-let my_public_key = my_private_key.compute_public_key().unwrap();
+let _my_public_key = my_private_key.compute_public_key().unwrap();
 
-let peer_public_key = {
+let peer_private_key = ring::agreement::EphemeralPrivateKey::generate(&algo, &rng).unwrap();
+let peer_public_key =  peer_private_key.compute_public_key().unwrap();
+let peer_public_key = ring::agreement::UnparsedPublicKey::new(&algo, peer_public_key);
+
     // In a real application, the peer public key would be parsed out of a
     // protocol message. Here we just generate one.
-    let peer_public_key = {
-        let peer_private_key =
-            EphemeralPrivateKey::generate(&algo, &rng).unwrap();
-        peer_private_key.compute_public_key().unwrap();
-    };
 
-    ring::agreement::UnparsedPublicKey::new(&algo, peer_public_key)
-};
-
-agree_ephemeral(
+ring::agreement::agree_ephemeral(
     my_private_key,
-    algo,
-    peer_public_key,
+    &peer_public_key,
     ring::error::Unspecified,
     |_key_material| {
         // In a real application, we'd apply a KDF to the key material and the
@@ -108,7 +100,7 @@ fn test_digest(data :&[u8], aglo: &'static ring::digest::Algorithm ){
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let datalength: u32 = rng.gen_range(0, 1000);
+    let datalength: usize = rng.gen_range(0, 1000);
     //println!("Data lenght is {}", datalength);
 
     let mut content = Vec::new();
@@ -124,12 +116,11 @@ fn main() {
     println!("Data: {}", BASE64.encode(data));
 
     //println!("CHACHA20_POLY1305");
-    test_aead(key,data,&CHACHA20_POLY1305);
-    //println!("AES_128_GCM");
+    test_aead(key,data,datalength,&CHACHA20_POLY1305);
     //Note: 128 bit key not 256 like the other two aeads
-    test_aead(&key[0..16],data,&AES_128_GCM);
+    test_aead(&key[0..16],data,datalength,&AES_128_GCM);
     //println!("AES_256_GCM");
-    test_aead(key,data,&AES_256_GCM);
+    test_aead(key,data,datalength,&AES_256_GCM);
 
     test_agreement(data,&ECDH_P256);
     test_agreement(data,&ECDH_P384);
