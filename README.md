2f801a6622e9b99bb707c7d6e44f185ceb7c3b40
Stephen Battista <stephen.battista@gmail.com>
2019-08-04 08:55:59 -0400

    Converted fixed length key. Debating overwrite of key with random rather than hash

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 [![Safety Dance](https://img.shields.io/badge/unsafe-forbidden-success.svg)](https://github.com/rust-secure-code/safety-dance/)
-
+<BR>
 Does not test nonce advance as we want to try an unlimited number of attempts. Uses less_safe_key in aead.
+<BR>
+Built with untrusted = "0.6.2"  Using "0.7.0" causes errors<BR>
+Built with ring = "0.16.5"<BR>
 
-Built with untrusted = "0.6.2"  Using "0.7.0" causes errors
-Built with ring = "0.16.5"
 
-PBKDF2 function lenght is capped at 100 (remainder of bytelength)
-Most keys are hashes of the byte input
-Most salts are hashes of the byte input or key.
+Not testing  the wiring of PKS8 files to disk. Just keeping them in memory.<BR>
+In aead nonce is first 12 bytes of the key.<BR>
+
+zero legnth input can cause crashes. (PBKDF2 random1 or random2). There needs to also be a check for length of output<BR>
--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use ring::signature::{ECDSA_P256_SHA256_FIXED_SIGNING,ECDSA_P256_SHA256_FIXED,EC
 use ring::signature::{ECDSA_P384_SHA384_FIXED_SIGNING,ECDSA_P384_SHA384_FIXED,ECDSA_P384_SHA384_ASN1_SIGNING,ECDSA_P384_SHA384_ASN1};
 
 //use ring::test::rand as testrand;
-//use data_encoding::BASE64;
+use data_encoding::BASE64;
 extern crate arrayref;
 
-fn test_aead(key: &[u8], data: &[u8], random1: &[u8], algo: &'static ring::aead::Algorithm) {
+fn test_aead(key: &[u8], data: &[u8], _random1: &[u8], algo: &'static ring::aead::Algorithm) {
     // Ring uses the same input variable as output
     let mut in_out = data.to_vec();
 
@@ -31,7 +31,7 @@ fn test_aead(key: &[u8], data: &[u8], random1: &[u8], algo: &'static ring::aead:
 
     let nonce_byte = &mut [0; 12];
 
-    nonce_byte.copy_from_slice(&random1[0..12]);
+    nonce_byte.copy_from_slice(&key[0..12]);
 
     // Encrypt data into in_out variable
     ring::aead::LessSafeKey::seal_in_place_append_tag(
@@ -56,11 +56,11 @@ fn test_aead(key: &[u8], data: &[u8], random1: &[u8], algo: &'static ring::aead:
     //println!("{}", BASE64.encode(&decrypted_data[..datalength]));
     assert_eq!(data[..], decrypted_data[..data.len()]);
 }
-fn test_agreement(random1: &[u8], keylenth: usize, algo: &'static ring::agreement::Algorithm) {
+fn test_agreement(key: &[u8], algo: &'static ring::agreement::Algorithm) {
     //let rng = ringrand::SystemRandom::new();
     //TODO make the key a SHA512 hash of data then take values
     let rng = ring::test::rand::FixedSliceRandom {
-        bytes: &random1[0..keylenth],
+        bytes: &key,
     };
     let my_private_key = ring::agreement::EphemeralPrivateKey::generate(&algo, &rng).unwrap();
 
@@ -111,14 +111,14 @@ impl From<ring::hkdf::Okm<'_, My<usize>>> for My<Vec<u8>> {
 }
 fn test_hkdf(
     data: &[u8],
-    random1: &[u8],
+    _random1: &[u8],
     random2: &[u8],
     key: &[u8],
     algo: &'static ring::hkdf::Algorithm,
 ) {
     //println!("datalen: {} , random1len {} , random2len {}, keylen {}",data.len(),random1.len(),random2.len(),key.len());
     let salt = ring::hkdf::Salt::new(*algo, key);
-    let prk = salt.extract(random1);
+    let prk = salt.extract(key);
     let My(_out) = prk.expand(&[&data], My(random2.len())).unwrap().into();
     //println!("HDKF: {}", BASE64.encode(out.as_ref()));
 }
@@ -171,7 +171,34 @@ fn test_ed25519(data: &[u8], key: &[u8]) {
     peer_public_key.verify(data, sig.as_ref()).unwrap();
 }
 
-fn test_ecdsa(data: &[u8], random1: &[u8],keylength : usize, signalgo : &'static ring::signature::EcdsaSigningAlgorithm , verifyalgo :&'static ring::signature::EcdsaVerificationAlgorithm) {
+fn test_ecdsa(data: &[u8], key: &[u8], signalgo : &'static ring::signature::EcdsaSigningAlgorithm , verifyalgo :&'static ring::signature::EcdsaVerificationAlgorithm) {
+    println!("{}",BASE64.encode(key.as_ref()));
+    let rng = ring::test::rand::FixedSliceRandom { bytes: &key};
+    println!("{}",BASE64.encode(key.as_ref()));
+    println!("here3");
+    let pkcs8_bytes = ring::signature::EcdsaKeyPair::generate_pkcs8(signalgo,&rng).unwrap();
+    println!("here4");
+    // Normally the application would store the PKCS#8 file persistently. Later
+    // it would read the PKCS#8 file from persistent storage to use it.
+
+    let key_pair = ring::signature::EcdsaKeyPair::from_pkcs8(signalgo, pkcs8_bytes.as_ref()).unwrap();
+
+    let sig = key_pair.sign(&rng,data).unwrap();
+
+    // Normally an application would extract the bytes of the signature and
+    // send them in a protocol message to the peer(s). Here we just get the
+    // public key key directly from the key pair.
+    let peer_public_key_bytes = key_pair.public_key().as_ref();
+
+    // Verify the signature of the message using the public key. Normally the
+    // verifier of the message would parse the inputs to this code out of the
+    // protocol message(s) sent by the signer.
+    let peer_public_key =
+        ring::signature::UnparsedPublicKey::new(verifyalgo, peer_public_key_bytes);
+    peer_public_key.verify(data, sig.as_ref()).unwrap();
+}
+
+/*fn test_rsa(data: &[u8], random1: &[u8],keylength : usize, signalgo : &'static ring::signature::RsaParameters:: , verifyalgo :&'static ring::signature::RsaVerificationAlgorithm) {
     let rng = ring::test::rand::FixedSliceRandom { bytes: &random1[..keylength] };
     let pkcs8_bytes = ring::signature::EcdsaKeyPair::generate_pkcs8(signalgo,&rng).unwrap();
 
@@ -193,14 +220,15 @@ fn test_ecdsa(data: &[u8], random1: &[u8],keylength : usize, signalgo : &'static
     let peer_public_key =
         ring::signature::UnparsedPublicKey::new(verifyalgo, peer_public_key_bytes);
     peer_public_key.verify(data, sig.as_ref()).unwrap();
-}
+} */
+
 fn main() {
     for x in 0..100 {
         println!("Attempt {}", x);
         let mut rng = rand::thread_rng();
 
-        let datalength: usize = rng.gen_range(32, 1000);
-        //32 is needed to seed the seedable rng
+        let datalength: usize = rng.gen_range(1, 500);
+        //Todo check if I can use 0
 
         let mut content = Vec::new();
         for _ in 0..datalength {
@@ -211,10 +239,17 @@ fn main() {
 
         //println!("Data: {}", BASE64.encode(data));
 
-        let mut rng: rand::rngs::StdRng =
-            rand::SeedableRng::from_seed(*arrayref::array_ref!(data, 0, 32));
+        let mut context = ring::digest::Context::new(&SHA384);
+        context.update(data);
+        let digest = context.finish();
+        let key =digest.as_ref();
+        println!("{}",key.len());
+        println!("{}",BASE64.encode(key.as_ref()));
 
-        let randomlen = rng.gen_range(48, 500); //Needed for agreement function
+        let mut rng: rand::rngs::StdRng =
+            rand::SeedableRng::from_seed(*arrayref::array_ref!(&key[..32], 0, 32));
+
+        let randomlen = rng.gen_range(1, 500);
         let mut content = Vec::new();
         for _ in 0..randomlen {
             let value: u8 = rng.gen();
@@ -224,7 +259,7 @@ fn main() {
         //println!("random1len: {}", random1.len());
         //println!("random1: {}", BASE64.encode(random1));
 
-        let randomlen = rng.gen_range(1, 500);
+        let randomlen = rng.gen_range(1, 500); //Needs to be non-zero
         let mut content = Vec::new();
         for _ in 0..randomlen {
             let value: u8 = rng.gen();
@@ -234,52 +269,52 @@ fn main() {
         //println!("random2len: {}", random2.len());
         //println!("random2: {}", BASE64.encode(random2));
 
-        let mut content = Vec::new();
-        for _ in 0..32 {
+        /*let mut content = Vec::new();
+        for _ in 0..48{
             let value: u8 = rng.gen();
             content.push(value);
         }
-        let key: &[u8] = content.as_ref();
+        let key: &[u8] = content.as_ref(); */
 
-        test_aead(key, data, random1, &CHACHA20_POLY1305);
+        test_aead(&key[..32], data, random1, &CHACHA20_POLY1305);
         //Note: 128 bit key not 256 like the other two aeads
         test_aead(&key[0..16], data, random1, &AES_128_GCM);
-        test_aead(key, data, random1, &AES_256_GCM);
+        test_aead(&key[..32], data, random1, &AES_256_GCM);
         //println!("done aead");
 
-        test_agreement(random1, 32, &ECDH_P256);
-        test_agreement(random1, 48, &ECDH_P384);
-        test_agreement(random1, 32, &X25519);
+        test_agreement(&key[..32],&ECDH_P256);
+        test_agreement(&key[..48],&ECDH_P384);
+        test_agreement(&key[..32],&X25519);
         //println!("done agreement");
 
         test_digest(data, &SHA256);
-        test_digest(data, &SHA384);
+        //test_digest(data, &SHA384); Used in generation of key.
         test_digest(data, &SHA512);
         test_digest(data, &SHA512_256);
         //println!("done digest");
 
-        test_hkdf(data, random1, random2, key, &HKDF_SHA256);
-        test_hkdf(data, random1, random2, key, &HKDF_SHA384);
-        test_hkdf(data, random1, random2, key, &HKDF_SHA512);
+        test_hkdf(data, random1, random2, &key[..32], &HKDF_SHA256);
+        test_hkdf(data, random1, random2, &key[..32], &HKDF_SHA384);
+        test_hkdf(data, random1, random2, &key[..32], &HKDF_SHA512);
         //println!("done hkdf");
 
-        test_hmac(data, key, &HMAC_SHA256);
-        test_hmac(data, key, &HMAC_SHA384);
-        test_hmac(data, key, &HMAC_SHA512);
+        test_hmac(data, &key[..32], &HMAC_SHA256);
+        test_hmac(data, &key[..32], &HMAC_SHA384);
+        test_hmac(data, &key[..32], &HMAC_SHA512);
         //println!("done hmac");
 
         test_pbkdf2(data, random1, random2, &PBKDF2_HMAC_SHA256);
         test_pbkdf2(data, random1, random2, &PBKDF2_HMAC_SHA384);
         test_pbkdf2(data, random1, random2, &PBKDF2_HMAC_SHA512);
-        //println!("done pbkdf2");
+        println!("done pbkdf2");
 
-        test_ecdsa(data, random1,32,&ECDSA_P256_SHA256_FIXED_SIGNING,&ECDSA_P256_SHA256_FIXED);
-        test_ecdsa(data, random1,32,&ECDSA_P256_SHA256_ASN1_SIGNING,&ECDSA_P256_SHA256_ASN1);
-        test_ecdsa(data, random1,48,&ECDSA_P384_SHA384_FIXED_SIGNING,&ECDSA_P384_SHA384_FIXED);
-        test_ecdsa(data, random1,48,&ECDSA_P384_SHA384_ASN1_SIGNING,&ECDSA_P384_SHA384_ASN1);
+        test_ecdsa(data, &key[..32],&ECDSA_P256_SHA256_FIXED_SIGNING,&ECDSA_P256_SHA256_FIXED);
+        test_ecdsa(data, &key[..32],&ECDSA_P256_SHA256_ASN1_SIGNING,&ECDSA_P256_SHA256_ASN1);
+        test_ecdsa(data, &key,&ECDSA_P384_SHA384_FIXED_SIGNING,&ECDSA_P384_SHA384_FIXED);
+        test_ecdsa(data, &key,&ECDSA_P384_SHA384_ASN1_SIGNING,&ECDSA_P384_SHA384_ASN1);
         println!("done ecdsa");
 
-        test_ed25519(data, key);
+        test_ed25519(data, &key[..32]);
         println!("done ed25519");
     }
 }
